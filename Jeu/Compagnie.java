e98e6abacd1d2addca1beba52a1ceeb428ac5505
rousssyl <rousssyl@pc-dg-027-07>
2016-01-05 10:57:58 +0100

    Sylvain

--- Jeu/Compagnie.java
+++ Jeu/Compagnie.java
@@ -15,11 +15,13 @@ public class Compagnie extends CarreauPropriete {
 	 * (d1 + d2) * mod
 	 * @param d1
 	 * @param d2
-	 * @param mod c'est quoi ça déjà ? TODO le rennomer comme il faut
+	 * @param mod c'est quoi ça déjà ? TODO le rennomer comme il faut 
+         *          --> c'est le modulo qui dépend du nombre de compagnie que le joueur possède. (1 compagnie -> mod=4 / 2 compagnies -> mod=10)
 	 */
 	public int calculPrixLoyerCompagnie(int d1, int d2, int mod) {
 		// TODO - implement Compagnie.calculPrixLoyerCompagnie
-		throw new UnsupportedOperationException();
+            return (d1 +d2) * mod;
+		//throw new UnsupportedOperationException();
 	}
 
     @Override //TODO  faire la fonction
@@ -29,7 +31,34 @@ public class Compagnie extends CarreauPropriete {
 
     @Override
     public int calculLoyer() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Joueur jProprio;
+        Joueur jCourant;
+        Monopoly monopoly;
+        int d1;
+        int d2;
+        int nbComp;
+        int loyer;
+        int ca;
+        int caRe;
+        
+        jProprio = this.getProprietaire();
+        monopoly = this.getMonopoly();
+        jCourant = monopoly.getJoueurCourant();
+        
+        d1 = monopoly.getDe1();
+        d2 = monopoly.getDe2();
+        nbComp = jProprio.getNbCompagnie();
+        ca = jCourant.getCash();
+        if (nbComp > 1){
+            loyer = this.calculPrixLoyerCompagnie(d1, d2, 10);
+        }else{
+            loyer = this.calculPrixLoyerCompagnie(d1, d2, 4);
+        }
+        caRe = jCourant.calculCashRestant(ca, loyer);
+        
+        monopoly.messageApresLoyer(jProprio, loyer, caRe);
+        return loyer ;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
